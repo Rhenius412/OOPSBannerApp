ccec8216a36ceda5feea169320ee9a108cf4bc52
Rhenius <rheniuskirubaharan@gmail.com>
2026-03-01 19:41:13 +0530

    UC3: Added String.join() implementation

--- UC3/OOPSBannerApp.java
+++ UC3/OOPSBannerApp.java
@@ -2,47 +2,13 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String line1 = String.join("    ",
-                "*****",
-                "*****",
-                "*****",
-                "*****");
-
-        String line2 = String.join("    ",
-                "*   *",
-                "*   *",
-                "*   *",
-                "*    ");
-
-        String line3 = String.join("    ",
-                "*   *",
-                "*   *",
-                "*   *",
-                "*    ");
-
-        String line4 = String.join("    ",
-                "*   *",
-                "*****",
-                "*****",
-                "*****");
-
-        String line5 = String.join("    ",
-                "*   *",
-                "*    ",
-                "*    ",
-                "*    ");
-
-        String line6 = String.join("    ",
-                "*   *",
-                "*    ",
-                "*    ",
-                "*    ");
-
-        String line7 = String.join("    ",
-                "*****",
-                "*****",
-                "*****",
-                "*****");
+        String line1 = String.join("   ", "*****", "*****", "*****", "*****");
+        String line2 = String.join("   ", "*   *", "*   *", "*   *", "*    ");
+        String line3 = String.join("   ", "*   *", "*   *", "*   *", "*    ");
+        String line4 = String.join("   ", "*   *", "*****", "*****", "*****");
+        String line5 = String.join("   ", "*   *", "*    ", "*    ", "    *");
+        String line6 = String.join("   ", "*   *", "*    ", "*    ", "    *");
+        String line7 = String.join("   ", "*****", "*    ", "*    ", "*****");
 
         System.out.println(line1);
         System.out.println(line2);
