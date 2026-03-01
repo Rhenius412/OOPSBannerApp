58a006f23b2f40415882faf38144e6a637ac12a0
Rhenius <rheniuskirubaharan@gmail.com>
2026-03-01 12:11:09 +0530

    UC1: Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+/**
+ * OOPSBannerApp
+ * 
+ * This class prints the literal text "OOPS" to the console.
+ * 
+ * @author Rhenius
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+
+}
