f3776d37ac425ae449349945b4b6940e136d3e5c
Rhenius <rheniuskirubaharan@gmail.com>
2026-03-16 09:53:56 +0530

    UC6: Implemented static helper methods for O, P, S patterns

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,16 +2,48 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] banner = {
-            "OOOO   OOOOO  PPPP    SSSS",
-            "O   O  O   O  P   P   S",
-            "O   O  O   O  PPPP    SSS",
-            "O   O  O   O  P          s",
-            "OOOO   OOOOO  P       SSSS"
-        };
+        String[] O = getO();
+        String[] P = getP();
+        String[] S = getS();
 
-        for (String line : banner) {
+        String[] banner = new String[5];
+
+        for(int i = 0; i < 5; i++) {
+            banner[i] = O[i] + "  " + O[i] + "  " + P[i] + "  " + P[i] + "  " + S[i] + "  " + S[i];
+        }
+
+        for(String line : banner) {
             System.out.println(line);
         }
+    }
+
+    static String[] getO() {
+        return new String[]{
+            " OOO ",
+            "O   O",
+            "O   O",
+            "O   O",
+            " OOO "
+        };
+    }
+
+    static String[] getP() {
+        return new String[]{
+            "PPPP ",
+            "P   P",
+            "PPPP ",
+            "P    ",
+            "P    "
+        };
+    }
+
+    static String[] getS() {
+        return new String[]{
+            " SSS ",
+            "S    ",
+            " SSS ",
+            "    S",
+            " SSS "
+        };
     }
 }
